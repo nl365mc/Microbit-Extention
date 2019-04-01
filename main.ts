f71a410d0c1eb1c76b94b80e850a0d10fc0d7524
nl365mc <31756813+nl365mc@users.noreply.github.com>
2019-04-01 12:22:15 -0500

    -tested function

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace basic {
+    export function yeet() {
+
+    }
+    export function Yee(v: number, interval: number = 150): void
+    { }
+}
