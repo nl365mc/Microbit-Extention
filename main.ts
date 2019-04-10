eecabaa6fe7f91c1e1940936a9a6a5205105b7b4
nl365mc <31756813+nl365mc@users.noreply.github.com>
2019-04-10 17:27:18 -0500

    -Changed Icon

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 
 
-//% color=300 weight=100 icon="\uf1ec" block="NL's Blocks"
+//% color=300 weight=100 icon="\f121" block="NL's Blocks"
 namespace NL {
-    
+
     //% blockId=yeet
-    //% block="show|yeet %v"
+    //% block="yeet %v"
     export function yeet() {
         basic.showString("Yeet")
     }
