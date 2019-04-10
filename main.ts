48810f5879ddb1d998be5db889e1c3e48a991380
nl365mc <31756813+nl365mc@users.noreply.github.com>
2019-04-10 17:05:39 -0500

    -Trying to make blocks work

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 //% color=300 weight=100 icon="\uf1ec" block="NL's Blocks"
 namespace basic {
 
-
+    //% blockId=yeet
+    //% block="show|yeet %v"
     export function yeet() {
         showString("Yeet")
     }
